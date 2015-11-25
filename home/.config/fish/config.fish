90bf7b99f531ad8f50f34a7b4e33e623564fcb1c
David Franklin <dgfranklin@google.com>
2015-11-25 14:56:32 -0500

    Update config.fish to support locally sourcing files

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,19 +2,20 @@ fish_vi_mode
 set fish_bind_mode insert
 set fish_key_bindings fish_user_key_bindings
 
-set -x FZF_DEFAULT_COMMAND /google/data/ro/users/lu/lutzky/fzf
 set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set -x EDITOR vim
 set -x P4DIFF "vimdiff -R"
 set -x P4MERGE vimdiff
-set -x PATH \
-    $HOME/.linuxbrew/bin \
-    $HOME/bin \
-    $PATH
-set -x MANPATH $HOME/.linuxbrew/share/man $MANPATH
-set -x INFOPATH $HOME/.linuxbrew/share/info $INFOPATH
 
 # Load fishmarks (http://github.com/techwizrd/fishmarks)
-source ~/.fishmarks/marks.fish
+if test -e $HOME/.fishmarks/marks.fish
+    source $HOME/.fishmarks/marks.fish
+end
 
-source "$HOME/.homesick/repos/homeshick/homeshick.fish"
+if test -e "$HOME/.homesick/repos/homeshick/homeshick.fish"
+    source "$HOME/.homesick/repos/homeshick/homeshick.fish"
+end
+
+if test -e $HOME/.config/fish/config_local.fish
+  source $HOME/.config/fish/.config_local.fish
+end
