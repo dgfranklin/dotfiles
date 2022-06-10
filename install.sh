dbca40b185f3de5e45c9c5282e36475466b9487f
David Franklin <david@dgfranklin.com>
2022-06-09 20:53:20 -0700

    Update install script path for homeshick

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ ! -f $HOME/.homesick/repos/homeshick/homeshick.sh ]]; then
-  git clone git@github.com:andsens/homeshick.git $HOME/.homesick/repos/homeshick
+  git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
 source $HOME/.homesick/repos/homeshick/homeshick.sh
 homeshick clone git@github.com:dgfranklin/dotfiles.git
